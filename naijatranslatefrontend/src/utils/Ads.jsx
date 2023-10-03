9b4899bb25d346263ee7ce332c5994e0c2cc7b6a
sam4rano <oyerindesamuelabiodun@gmail.com>
2023-10-03 16:01:31 +0100

    ads props completed

--- naijatranslatefrontend/src/utils/Ads.jsx
+++ naijatranslatefrontend/src/utils/Ads.jsx
@@ -1,15 +1,18 @@
-//import { Bix} from "react-icons/bi";
 
-const Ads = () => {
+import cancel from "../assets/cancel.svg";
+
+const Ads = ({toggleDivVisibility}) => {
+  
   return (
-    <div className="w-full h-[35px]  bg-[#FFE680]">
+    <div className="w-full h-[35px] flex flex-row justify-around  bg-light" onClick={toggleDivVisibility}>
       <h1 className="mx-auto text-center p-sm">
         Translate like a pro with our higher percentage translation solution
       </h1>
- 
-
+      <div className="w-[30px] h-[30px]" >
+        <img src={cancel} alt="cancel" className="w-[40px] h-[40px] py-[10px] cursor-pointer" /> 
+      </div>
     </div>
   );
-}
+};
 
-export default Ads
+export default Ads;
