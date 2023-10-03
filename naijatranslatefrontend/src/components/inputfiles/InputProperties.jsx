0e6b2f547cdc442591d9338fe071c1cec800fd68
sam4rano <oyerindesamuelabiodun@gmail.com>
2023-10-03 15:58:42 +0100

    input properties assets

--- naijatranslatefrontend/src/components/inputfiles/InputProperties.jsx
+++ naijatranslatefrontend/src/components/inputfiles/InputProperties.jsx
@@ -3,12 +3,12 @@ import inspeaker from "../../assets/Inspeaker.svg"
 const InputProperties = () => {
   return (
     <div className="flex flex-row justify-between px-[10px]">
-      <div className="flex flex-row justify-around rounded-full px-[5px] h-[30px] border-[1px]">
+      <div className="flex flex-row justify-around rounded-full px-[5px] h-[30px] border-[1px] border-outline">
         
         <img src={inspeaker} className="w-[30px] h-[30px]" alt="speaker"/>
         <h3 className="pt-[2px] ">speak</h3>
       </div>
-      <h1 className="text-center">0/2mins</h1>
+      <h3 className="text-center pt-[6px]">0/2mins</h3>
     </div>
   );
 };
