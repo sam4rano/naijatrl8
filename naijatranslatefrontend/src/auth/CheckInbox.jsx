750473fc013ff32e7a892e484928f6bd16671676
sam4rano <oyerindesamuelabiodun@gmail.com>
2023-10-11 17:47:39 +0100

    routing issue resolved

--- naijatranslatefrontend/src/auth/CheckInbox.jsx
+++ naijatranslatefrontend/src/auth/CheckInbox.jsx
@@ -12,12 +12,21 @@ const CheckInbox = () => {
 
         <div className="flex flex-col">
           <h1 className="pb-[10px] text-center">Verification not received?</h1>
-          
+          <div className="flex flex-row w-[200px] mx-auto">
           <Link to="/resendverification" className="bg-primary p-[10px] text-white rounded-full hover:bg-transparent text-center">
             <span className="rounded-[10px]">
               Request new verification Link
             </span>
           </Link>
+          <Link to="/logincontainer" className="bg-primary p-[10px] text-white rounded-full hover:bg-transparent text-center">
+            <span className="rounded-[10px]">
+              Login
+            </span>
+          </Link>
+
+          </div>
+         
+
         </div>
       </div>
     </div>
